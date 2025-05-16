6d15379a15995dc040e4ad5ef6b8d1a6ad6c76d7
itc-s24011 <s24011@std.it-college.ac.jp>
2025-05-16 12:40:21 +0900

    課題

---
+++ D48Q25.java
@@ -0,0 +1,13 @@
+public class D48Q25 {
+    String code;
+    public static void main(String[] args) {
+        D48Q25 obj = new D48Q25();
+        obj.setCode("F81257");
+    }
+    void  setCode(String code) {
+        this.code = code;
+    }
+    String getCode() {
+        return this.code;
+    }
+}
