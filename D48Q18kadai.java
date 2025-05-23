92dd14652f0f3f382e278484be86dd1f422185de
itc-s24011 <s24011@std.it-college.ac.jp>
2025-05-23 11:15:23 +0900

    課題

--- D48Q18kadai.java
+++
@@ -1,6 +0,0 @@
-public class D48Q18kadai {
-    public static void main(String[] args) {
-        double n = 20.0;
-        System.out.println(n * 2); // 40.0 を出力
-    }
-}
--- D49Q17kadai.java
+++ D49Q17kadai.java
@@ -1,4 +1,4 @@
-public class D48Q17kadai {
+public class D49Q17kadai {
     public static void main(String[] args) {
         int n = 1;
         System.out.println(++n + " " + --n );
