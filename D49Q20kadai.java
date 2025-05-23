ceea1ad234d2fb4cb51553e8646ca6ee80d8e49a
itc-s24011 <s24011@std.it-college.ac.jp>
2025-05-23 12:08:46 +0900

    課題

---
+++ D49Q20kadai.java
@@ -0,0 +1,7 @@
+public class D49Q20kadai {
+    public static void main(String[] args) {
+        int[][] arr = new int[10][5];
+
+        System.out.println(arr.length);
+    }
+}
---
+++ D49Q21.java
@@ -0,0 +1,8 @@
+public class D49Q21 {
+    public static void main(String args[]) {
+        int n1 = 0x40;
+        int n2 = 0x10;
+
+        System.out.println(n1 + n2);
+    }
+}
