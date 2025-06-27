99fc8f0700b5b075f3edae5b72f2c86936432dac
itc-s24011 <s24011@std.it-college.ac.jp>
2025-06-27 12:26:11 +0900

    課題

---
+++ D51Q23kadai.java
@@ -0,0 +1,11 @@
+public class D51Q23kadai {
+        public static void main(String[] args) {
+            // 季節の配列を定義
+            String[] seasons = {"春", "夏", "秋", "冬"};
+
+            // 拡張for文を使って季節を出力
+            for (String season : seasons) {
+                System.out.print(season + "。");
+            }
+        }
+    }
